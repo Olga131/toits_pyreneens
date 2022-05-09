f9867ce29b003496164edf8487a5d278084205b7
Vasyutynska <ovas131@gmail.com>
2022-05-09 17:16:23 +0200

    kk

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,23 @@
 import '../css/app.scss';
 
-const li1 = document.getElementsByClassName("li1");
+const li1 = document.getElementById("li1");
 console.log(li1);
-const li2 = document.getElementsByClassName("li2");
-const li3 = document.getElementsByClassName("li3");
-const li4 = document.getElementsByClassName("li4");
-const li5 = document.getElementsByClassName("li5");
-const li6 = document.getElementsByClassName("li6");
-const li7 = document.getElementsByClassName("li7");
-const li8 = document.getElementsByClassName("li8");
+const li2 = document.getElementById("li2");
+const li3 = document.getElementById("li3");
+const li4 = document.getElementById("li4");
+const li5 = document.getElementById("li5");
+const li6 = document.getElementById("li6");
+const li7 = document.getElementById("li7");
+const li8 = document.getElementById("li8");
 
-const div1 = document.getElementsByClassName("div1");
-const div2 = document.getElementsByClassName("div2");
-const div3 = document.getElementsByClassName("div3");
-const div4 = document.getElementsByClassName("div4");
-const div5 = document.getElementsByClassName("div5");
-const div6 = document.getElementsByClassName("div6");
-const div7 = document.getElementsByClassName("div7");
-const div8 = document.getElementsByClassName("div8");
+const div1 = document.getElementById("div1");
+const div2 = document.getElementById("div2");
+const div3 = document.getElementById("div3");
+const div4 = document.getElementById("div4");
+const div5 = document.getElementById("div5");
+const div6 = document.getElementById("div6");
+const div7 = document.getElementById("div7");
+const div8 = document.getElementById("div8");
 console.log(div4);
 
 function showContent(obj) {
@@ -25,7 +25,7 @@ function showContent(obj) {
     if (obj.style.display === "none") {
         obj.style.display = "block";
       } else {
-        x.style.display = "none";
+        obj.style.display = "none";
       }
     } 
 
@@ -39,3 +39,4 @@ li6.addEventListener('click', function() {showContent(div6)});
 li7.addEventListener('click', function() { showContent(div7)});
 li8.addEventListener('click', function() {showContent(div8)});
 
+console.log(obj);
